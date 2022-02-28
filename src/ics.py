f42f70280131a4bd4e7b2407fc2c4ae978dfa3d2
Aniket Gargya <gargya.aniket@gmail.com>
2022-02-28 02:49:09 -0600

    :art: use docopt for ics.py

--- src/ics.py
+++ src/ics.py
@@ -1,23 +1,27 @@
+'''ics.py
+
+Usage:
+    ics.py add <link>
+    ics.py read
+    ics.py (-h | --help)
+
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+'''
+from docopt import docopt
 import sys
 import ics_reader
 
 filename = 'ical_links.txt'
 
-if sys.argv[1] != "-add" and sys.argv[1] != "-read" and sys.argv[1] != "-help":
-    print("ERROR: Unknown Command. Type -help for more information")
-elif sys.argv[1] == "-help":
-    print("Commands:")
-    print("-add [link]\tadds a link to %s" % filename)
-    print("-read\t\treads all ical links stored in %s" % filename)
-elif sys.argv[1] == "-add" and len(sys.argv) != 3:
-    print("Usage: python ics.py -add [link]")
-elif sys.argv[1] == "-add":
-    # writing to file
+def add(link):
     file1 = open(filename, 'a')
-    file1.writelines(sys.argv[2])
+    file1.writelines(link)
     file1.close()
     print("Added Link to Source File")
-elif sys.argv[1] == "-read":
+
+def read():
     try:
         ofs = open(filename, 'r')
     except IOError:
@@ -27,3 +31,14 @@ elif sys.argv[1] == "-read":
         event_strings = ics_reader.ReadICal(read_lines)
         for line in event_strings:
             print(line + '\n')
+
+def main(arguments):
+    if arguments['add']:
+        add(arguments['<link>'])
+    elif arguments['read']:
+        read()
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='ics.py 1.0')
+    main(arguments)
+    
