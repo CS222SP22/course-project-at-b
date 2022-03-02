a323162b2a32eef0fb9b2a4fcd70ea27d3747358
Pranav Chandra <pranavc5@illinois.edu>
2022-03-02 15:16:49 -0600

    made edits from Ajay Feedback 03/02/2022 2:00 PM

--- src/ics_reader.py
+++ src/ics_reader.py
@@ -14,31 +14,26 @@ Takes the event input and returns the desired atributes of that event
     - End Date (dtend)
     - Next Time event occurs if a repeating rule exists (nextrule)
 """
-def GetEventData(event, date, tz):
+def getEventData(event, date, tz):
     # if a start date attribute exists for event, assign it to dtstart
     #   else set dtstart to false to show no start date
     if 'DTSTART' in event:
-        try:
-            dtstart = event['DTSTART'].dt.astimezone(tz) # assign time in chosen timezone
-        except:
-            dtstart = False
+        dtstart = event['DTSTART'].dt.astimezone(tz) # assign time in chosen timezone
+    else:
+        dtstart = False
     # if an end date attribute exists for event, assign it to dtend
     #   else set dtend to false to show no end date
     if 'DTEND' in event:
-        try:
-            dtend = event['DTEND'].dt.astimezone(tz) # assign time in chosen timezone
-        except:
-            dtend = False
+        dtend = event['DTEND'].dt.astimezone(tz) # assign time in chosen timezone
+    else:
+        dtend = False
     # if a repeating rule exists, calculate the next time the rule exists after today
     #   and assign it to next rule
     #   else set nextrule to false to show no repeating rule
     if 'RRULE' in event:
-        try:
-            ruletext = event['RRULE'].to_ical().decode()
-            rule = rrule.rrulestr(ruletext, dtstart=event['DTSTART'].dt)
-            nextrule=rule.after(date.astimezone(tz)) # assign time in chosen timezone
-        except:
-            nextrule = False
+        ruletext = event['RRULE'].to_ical().decode()
+        rule = rrule.rrulestr(ruletext, dtstart=event['DTSTART'].dt)
+        nextrule=rule.after(date.astimezone(tz)) # assign time in chosen timezone
     else:
         nextrule = False
     
@@ -51,25 +46,27 @@ EventToString(event, date, tz)
 Takes the event input and returns the event as a string version
 String has the summary title of the event along with start and end times
 """
-def EventToString(event, date, tz):
+def eventToString(event, date, tz):
     # create empty return value string
     event_string = ""
 
     # obtain event data using GetEventData()
-    dtstart, dtend, nextrule = GetEventData(event, date, tz)
+    dtstart, dtend, nextrule = getEventData(event, date, tz)
 
-    # if event has datatypes we should report
-    if dtstart or dtend or nextrule:
-        # add summary/title of the event to return string
-        event_string += str(event['summary']).strip()
+    # if event does not have datatypes we should report, we return empty
+    if not (dtstart or dtend or nextrule):
+        return ""
 
-        # if repeating rule does not exist, report start and end dates
-        #   else report repeating rule date and end time after rule date
-        if not nextrule:
-                event_string += ("\n%s to %s" % (dtstart, dtend))
-        else:
-            length = (dtend - dtstart).total_seconds() / 60.0
-            event_string += ("\n%s to %s" % (nextrule, nextrule + length))
+    # add summary/title of the event to return string
+    event_string += str(event['summary']).strip()
+
+    # if repeating rule does not exist, report start and end dates
+    #   else report repeating rule date and end time after rule date
+    if not nextrule:
+            event_string += ("\n%s to %s" % (dtstart, dtend))
+    else:
+        length = (dtend - dtstart).total_seconds() / 60.0
+        event_string += ("\n%s to %s" % (nextrule, nextrule + length))
 
     # return event as developed string
     return event_string
@@ -80,7 +77,7 @@ ReadICal(calendars)
 Takes a list of calendar links and returns a list of strings 
 Each string in the list contains the summary/title, start date, and end date of each event
 """
-def ReadICal(calendars):
+def readICal(calendars):
     # create empty return value list
     event_strings = []
     # define formate for time strings
@@ -103,7 +100,7 @@ def ReadICal(calendars):
         # each event in ical files start with "VEVENT"
         # so walk through ical file and stop at every instance of "VEVENT"
         for event in gcal.walk('VEVENT'):
-            event_strings.append(EventToString(event, date, tz))
+            event_strings.append(eventToString(event, date, tz))
     
     # return list of events as string
     return event_strings
