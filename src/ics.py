13e9c94b4959496708a5a46a0dc81f81f18c5558
Aniket Gargya <gargya.aniket@gmail.com>
2022-03-22 17:19:09 -0500

    :bug: fixing add function for case where json file doesnt exist

--- src/ics.py
+++ src/ics.py
@@ -1,13 +1,13 @@
 '''ics.py
 
 Usage:
-    ics.py add <link> <lms>
-    ics.py read
-    ics.py (-h | --help)
+	ics.py add <link> <lms>
+	ics.py read
+	ics.py (-h | --help)
 
 Options:
-    -h --help       Show this screen.
-    --version       Show version.
+	-h --help       Show this screen.
+	--version       Show version.
 '''
 from docopt import docopt
 import sys
@@ -17,6 +17,11 @@ import json
 filename = 'ical_links.json'
 
 def add(link, lms):
+	try:
+		open(filename, 'r').close()
+	except IOError:
+		create_file()
+		print('Unable to find file, creating it!')
 	with open(filename, 'r') as f:
 		data = json.load(f)
 	
@@ -43,33 +48,33 @@ def add(link, lms):
 	print('Added Link to Source File')
 
 def read(): 
-    try:
-        ofs = open(filename, 'r')
-    except IOError:
-        create_file()
-        print('Unable to find file, creating it!')
-    else:
-        data = json.load(ofs)
-        # iterate through links and generate event strings
-        for d in data['links']:
-            #TODO: Pass in the LMS, for the purpose of better filtering.
-            print(d)
-            event_strings = ics_reader.readICal(d['url'], d['lms'])
-            for line in event_strings:
-                print(line)
+	try:
+		ofs = open(filename, 'r')
+	except IOError:
+		create_file()
+		print('Unable to find file, creating it!')
+	else:
+		data = json.load(ofs)
+		# iterate through links and generate event strings
+		for d in data['links']:
+			#TODO: Pass in the LMS, for the purpose of better filtering.
+			print(d)
+			event_strings = ics_reader.readICal(d['url'], d['lms'])
+			for line in event_strings:
+				print(line)
 
 def create_file():
-    data = {'link-count':0, 'links':[]}
-    with open(filename, 'w+') as f:
-        json.dump(data, f)
+	data = {'link-count':0, 'links':[]}
+	with open(filename, 'w+') as f:
+		json.dump(data, f)
 
 def main(arguments):
-    if arguments['add']:
-        add(arguments['<link>'], arguments['<lms>'])
-    elif arguments['read']:
-        read()
+	if arguments['add']:
+		add(arguments['<link>'], arguments['<lms>'])
+	elif arguments['read']:
+		read()
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='ics.py 1.0')
-    main(arguments)
-    
+	arguments = docopt(__doc__, version='ics.py 1.0')
+	main(arguments)
+	
