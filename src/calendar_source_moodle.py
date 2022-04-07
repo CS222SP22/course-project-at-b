5381d91e7a37b0e3ab9c0b3add2df2feefc867eb
Pranav Chandra <pranavc5@illinois.edu>
2022-04-07 01:52:44 -0500

    included moodle in LMS list

--- src/calendar_source_moodle.py
+++ src/calendar_source_moodle.py
@@ -5,16 +5,17 @@ class Moodle(CalendarSourceTemplate):
     def extract_event_info(self, event, event_dictionary):
         event_dictionary['source_name'] = "moodle"
 
-        event_dictionary['name'] = event['summary'][6:]
+        event_dictionary['name'] = event['summary']
 
-        event_dictionary['type'] = 'Exam/Quiz'
+        event_dictionary['type'] = 'Needs to be manually sorted'
 
-        partitioned_summary = event['summary'][6:].partition(" ")	
-        event_dictionary['course'] = partitioned_summary[0] + " " + partitioned_summary[2].partition(" ")[0]
+        category_data = event.get('categories').to_ical().decode()
+        res = [i for i in range(len(category_data)) if category_data.startswith(" ", i)]
+        event_dictionary['course'] = category_data[:res[1]]
     
     def filter_event(self, event):
-        return not 'Exam' in event['summary']
+        return 'categories' in event
 
 # if __name__ == '__main__':
-#     my_moodle_source = Moodle('https://learn.illinois.edu/calendar/export_execute.php?userid=537403&authtoken=d30326f98ee928652597c810df4a268a7b531b84&preset_what=all&preset_time=custom')
+#     my_moodle_source = Moodle('https://learn.illinois.edu/calendar/export_execute.php?userid=545202&authtoken=88cb7b03f8f6317d5d16e8096a3eed8232773bb7&preset_what=all&preset_time=recentupcoming')
 #     print(json.dumps(my_moodle_source.request(), indent=4, default=str))
