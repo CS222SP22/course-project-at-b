bb3988d00b5b3f670d5f002feb8e5c108aa28215
Pranav Chandra <pranavc5@illinois.edu>
2022-04-11 23:48:19 -0500

    got all data into txt files after scraping. needs to be parsed and converted to dicts

--- src/calendar_source_prairie.py
+++ src/calendar_source_prairie.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup
+from scrapingant_client import ScrapingAntClient
 
 class Prairie(CalendarSourceTemplate):
     def extract_event_info(self, event, event_dictionary):
@@ -21,28 +22,32 @@ class Prairie(CalendarSourceTemplate):
         # Login to PL
         email = f.readline()    
         password = f.readline()    
+        driver.get("https://www.prairielearn.org/")
+        driver.find_element(By.LINK_TEXT, "Log in").click()
+        time.sleep(5) 
+        driver.find_element(By.LINK_TEXT, "Sign in with Illinois").click()
+        time.sleep(5) 
+        driver.find_element(By.ID, "userNameInput").send_keys(email + Keys.ENTER)
+        time.sleep(5) 
+        driver.find_element(By.ID, "passwordInput").send_keys(password + Keys.ENTER)
+        time.sleep(5) 
+    
         driver.get(self.link)
-        # driver.find_element(By.LINK_TEXT, "Log in").click()
-        # time.sleep(5) 
-        # driver.find_element(By.LINK_TEXT, "Sign in with Illinois").click()
-        # time.sleep(5) 
-        # driver.find_element(By.ID, "userNameInput").send_keys(email + Keys.ENTER)
-        # time.sleep(5) 
-        # driver.find_element(By.ID, "passwordInput").send_keys(password + Keys.ENTER)
-        # time.sleep(5) 
-
+        
         # Webscrape PL
+        # client = ScrapingAntClient(token='2b6ffb675d654a998e1d64e08953bdac')
+        # page_source = client.general_request(self.link).content
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
 
         assignments = []
         # get data on page
-        for assignment in soup.find_all('td'):
+        for assignment in soup.find_all(['td']):
             # display the actual urls
             # ass = assignment.get('href')
             # if ass.find("course_instance") != -1:
                 # assignments.append("https://www.prairielearn.org" + ass)
-            assignments.append(assignment)
+            assignments.append(assignment.get_text())
         
         test_file = open("data/" + self.link[48:] + ".txt", "w")
 
